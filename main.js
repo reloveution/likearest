b72ab2fe2e55257b46d77741475b3d4b70d4ca5c
Michael Kolinski <michaelkolinski150@gmail.com>
2022-12-02 03:16:28 +0400

    trouble with get customers in customerModel

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ import { SellerBuilder } from "./Builder/sellerBuilder.js"
 import { SellerArrayBuilder } from "./Builder/sellerBuilder.js"
 import { SellerModel } from "./Models/sellerModel.js"
 
+import { CustomerBuilder } from "./Builder/customerBuilder.js"
+import { CustomerArrayBuilder } from "./Builder/customerBuilder.js"
+import { CustomerModel } from "./Models/customerModel.js"
+
 import axios from 'axios';
+
+let sellerModel
+let customerModel
 
 // let sellerModel = axios.get('http://localhost:3000/seller').then(response =>{
 //     // console.log(response.data)
@@ -19,12 +26,19 @@ import axios from 'axios';
 //     console.log(SellerModel.sellerArrayBuilder(response.data).build());
 // });
 
-let sellerModel
+
 axios.get('http://localhost:3000/seller').then(response => {
     // console.log(response.data)
     // console.log(response.data.length)
     sellerModel = SellerModel.sellerArrayBuilder(response.data).build();
 });
+
+setTimeout(getCustomers, 3000);
+function getCustomers() {
+    axios.get('http://localhost:3000/customer').then(response => {
+        customerModel = CustomerModel.customerArrayBuilder(response.data).build();
+    });
+}
 
 // for (let time = 0; typeof(sellerModel[0]) == undefined; time++) {
 //     console.log(time)
@@ -33,5 +47,8 @@ axios.get('http://localhost:3000/seller').then(response => {
 
 setTimeout(listingDB, 3000);
 function listingDB() {
-  console.log(sellerModel);
+    console.log('----sellers----')
+    console.log(sellerModel);
+    console.log('----customers--')
+    console.log(customerModel)
 }
