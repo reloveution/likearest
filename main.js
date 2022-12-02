c04d815b5c8f552f3a44aeaef797200b684e388c
Michael Kolinski <michaelkolinski150@gmail.com>
2022-12-02 14:29:58 +0400

    friday 14:29

--- main.js
+++ main.js
@@ -27,28 +27,26 @@ let customerModel
 // });
 
 
-axios.get('http://localhost:3000/seller').then(response => {
+axios.get('http://localhost:3000/seller')
+.then(response => {
     // console.log(response.data)
     // console.log(response.data.length)
     sellerModel = SellerModel.sellerArrayBuilder(response.data).build();
+})
+.catch(error => {
+    console.log('Error with get sellers from DB')
 });
 
-setTimeout(getCustomers, 3000);
-function getCustomers() {
-    axios.get('http://localhost:3000/customer').then(response => {
-        customerModel = CustomerModel.customerArrayBuilder(response.data).build();
-    });
-}
 
-// for (let time = 0; typeof(sellerModel[0]) == undefined; time++) {
-//     console.log(time)
-// } 
+axios.get('http://localhost:3000/customer').then(response => {
+    customerModel = CustomerModel.customerArrayBuilder(response.data).build();
+}); 
 
 
-setTimeout(listingDB, 3000);
+setTimeout(listingDB, 3000)
 function listingDB() {
     console.log('----sellers----')
-    console.log(sellerModel);
+    console.log(sellerModel)
     console.log('----customers--')
     console.log(customerModel)
 }
